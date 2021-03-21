0cb85c86f659209605293bf38de82fb47baf51d6
Yura <kik_1234@mail.ru>
2021-03-21 23:30:03 +0300

    section: quations

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@ $(function () {
     $('.top__slider').slick({
         arrows: false,
         dots: true,
-        fade:true,
-        autoplay:true
+        fade: true,
+        autoplay: true
     });
-        
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
